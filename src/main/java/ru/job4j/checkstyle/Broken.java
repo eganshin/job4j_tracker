323e5c188162daab2622c88817e895bd8b9d9983
eganshin <iEganshin99@gmail.com>
2023-09-25 19:27:47 +0500

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEWVALUE = "";
+    private int sizeOfEmpty = 10;
+    public String surname;
+    String name;
+
+    void echo() {
+    }
+
+    Broken() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+}
